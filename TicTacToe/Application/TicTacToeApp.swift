5f5735ae865fd21156e1efdfad5127ba8ef0563f
Alexandr Rodionov <alexrodionby@gmail.com>
2024-09-28 22:43:56 +0300

    Добавил стартовый проект
    Добавлен стартовый проект, минимальная настройка, добавил иконку, гитигнор.

---
+++ TicTacToe/Application/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Alexandr Rodionov on 28.09.24.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            OnboardingView()
+        }
+    }
+}
