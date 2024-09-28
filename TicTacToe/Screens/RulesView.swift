c93de494389f4a3d0b7c15a754c78f38e71199ab
Alexandr Rodionov <alexrodionby@gmail.com>
2024-09-29 00:14:23 +0300

    Добавил заготовки для экранов
    Добавил примерный список пустых экранов

--- TicTacToe/Screens/RulesView.swift
+++ TicTacToe/Screens/RulesView.swift
@@ -1,18 +1,18 @@
 //
-//  OnboardingView.swift
+//  RulesView.swift
 //  TicTacToe
 //
-//  Created by Alexandr Rodionov on 28.09.24.
+//  Created by Alexandr Rodionov on 29.09.24.
 //
 
 import SwiftUI
 
-struct OnboardingView: View {
+struct RulesView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    OnboardingView()
+    RulesView()
 }
