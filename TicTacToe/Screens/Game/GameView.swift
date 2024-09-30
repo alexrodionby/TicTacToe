b757d9aa65604ff4a864389cc0a3e806e9aa4513
Alexandr Rodionov <alexrodionby@gmail.com>
2024-09-30 22:27:11 +0300

    Изменения в игровом движке
    Доработал игровой движок.

--- TicTacToe/Screens/Game/GameView.swift
+++ TicTacToe/Screens/Game/GameView.swift
@@ -11,47 +11,68 @@ struct GameView: View {
     
     @Environment(GameViewModel.self) private var gameVM
     
-    var columns: [GridItem] = [GridItem(.flexible(), spacing: 10), GridItem(.flexible()), GridItem(.flexible())]
-
+    var columns: [GridItem] = [
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
+    
     var gridSpacing: CGFloat = 20
     var cellCornerRadius: CGFloat = 20
+    var gridBackgroundCornerRadius: CGFloat = 30
     
     var body: some View {
-        GeometryReader { geometry in
-            VStack {
+        ZStack(alignment: .center) {
+            Color.customBackground
+                .ignoresSafeArea()
+            
+            ZStack(alignment: .center) {
                 LazyVGrid(columns: columns, alignment: .center, spacing: gridSpacing) {
                     ForEach(0..<9) { i in
                         ZStack(alignment: .center) {
                             RoundedRectangle(cornerRadius: cellCornerRadius, style: .continuous)
                                 .fill(.customLightBlue)
-                                .frame(width: geometry.size.width / 3 - gridSpacing + 2, height: geometry.size.width / 3 - gridSpacing, alignment: .center)
-                              
+                                .aspectRatio(1, contentMode: .fit)
                             
                             if let move = gameVM.moves[i] {
-                                if move.player == .playerOne {
+                                if move.player == gameVM.firstTurnPlayer {
                                     Image("xSkin5")
                                         .resizable()
-                                        .frame(width: 50, height: 50, alignment: .center)
+                                        .aspectRatio(1, contentMode: .fit)
+                                        .padding(20)
                                 } else {
                                     Image("oSkin5")
                                         .resizable()
-                                        .frame(width: 50, height: 50, alignment: .center)
+                                        .aspectRatio(1, contentMode: .fit)
+                                        .padding(20)
                                 }
                             }
                         }
+                        .frame(maxWidth: .infinity)
+                        .padding(.horizontal, 5)
                         .onTapGesture {
                             gameVM.processingPlayerMove(move: Move(player: gameVM.currentPlayer, boarderIndex: i))
                         }
                     }
+                    
                 }
+                .padding(20)
+            }
+            .background {
+                RoundedRectangle(cornerRadius: gridBackgroundCornerRadius, style: .continuous)
+                                        .fill(.customWhite)
+            }
+            .padding(40)
+            VStack {
+                Spacer()
                 Button {
                     gameVM.resetGame()
                 } label: {
                     Text("Reset")
                         .padding()
                 }
-
             }
+
         }
     }
 }
