660b39ee77ec5702571e432184a1df0ac06caf47
Alexandr Rodionov <alexrodionby@gmail.com>
2024-09-30 16:19:57 +0300

    Начал разработку логики игры с другим игроком
    Добавил базовую логику, начал верстку экрана игры.

--- TicTacToe/Components/BoardCell.swift
+++ TicTacToe/Components/BoardCell.swift
@@ -1,18 +1,18 @@
 //
-//  GameView.swift
+//  BoardCell.swift
 //  TicTacToe
 //
-//  Created by Alexandr Rodionov on 29.09.24.
+//  Created by Alexandr Rodionov on 30.09.24.
 //
 
 import SwiftUI
 
-struct GameView: View {
+struct BoardCell: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    GameView()
+    BoardCell()
 }
---
+++ TicTacToe/Screens/Game/GameViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  GameViewModel.swift
+//  TicTacToe
+//
+//  Created by Alexandr Rodionov on 30.09.24.
+//
+
+import Foundation
